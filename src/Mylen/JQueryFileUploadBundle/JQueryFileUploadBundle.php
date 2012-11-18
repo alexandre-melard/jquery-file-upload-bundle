3b7f5fb7fc6b68ba4254d638d1443d6265b62fc1
Alexandre Melard <git@melard.fr>
2012-11-18 07:50:37 +0100

    initial

---
+++ src/Mylen/JQueryFileUploadBundle/JQueryFileUploadBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mylen\JQueryFileUploadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class JQueryFileUploadBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new FormPass());
+        $container->addCompilerPass(new NavbarPass());
+    }
+}

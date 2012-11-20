7e88f4f89eac864cc2fdd355ed0e13077e6be2d1
Alexandre Melard <git@melard.fr>
2012-11-20 11:50:20 +0100

    added custom cssrewrite filter

--- DependencyInjection/JQueryFileUploadBundleExtension.php
+++ DependencyInjection/JQueryFileUploadBundleExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class PunkAveFileUploaderExtension extends Extension
+class JQueryFileUploadBundleExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -24,5 +24,6 @@ class PunkAveFileUploaderExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('filters.yml');
     }
 }
